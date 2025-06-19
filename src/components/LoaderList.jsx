d7232f5c9c7cbd6c692b72668f4ef5bef263d79e
Mauro Bonifazi <Maurobonifazi@hotmail.com>
2025-06-19 10:59:46 -0300

    Corregir sintaxis y hacer LoaderList responsive

--- src/components/LoaderList.jsx
+++ src/components/LoaderList.jsx
@@ -1,17 +1,18 @@
-// LoaderList.jsx
 import React from "react";
-import { Grid } from "@mui/material";
+import { Grid, Box } from "@mui/material";
 import Loader from "./Loader";
 
 function LoaderList({ count = 3 }) {
   return (
-    <Grid container spacing={2}>
-      {Array.from({ length: count }).map((_, index) => (
-        <Grid item xs={12} sm={6} md={4} key={index}>
-          <Loader />
-        </Grid>
-      ))}
-    </Grid>
+    <Box px={2} py={4}>
+      <Grid container spacing={2}>
+        {Array.from({ length: count }).map((_, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Loader />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
 
