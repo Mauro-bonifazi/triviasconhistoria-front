9dee355c235f60ca1abdffd91d5e4fbd3caf633e
Mauro Bonifazi <maurobonifazi@hotmail.com>
2026-04-20 18:31:09 -0300

    Feat: agregar Vercel Analytics al frontend

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { HelmetProvider } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
+      <Analytics />
     </HelmetProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
